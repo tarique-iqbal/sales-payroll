e09a9293d4d2c2e50c29f402c7f053fc20ab45c4
Tarique Iqbal <live.iqbal@gmail.com>
2019-03-16 02:03:01 +0100

    Add production params config

---
+++ config/params.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+$config = [
+    'data' => [
+        'directory'     => 'data'
+    ],
+
+    'log' => [
+        'directory'     => 'log',
+        'file_name'     => 'errors.log'
+    ],
+];
+
+return $config;
